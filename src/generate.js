444527ce51c14c7153c58ff69f2bc9dbe8b91193
0xJayden <gannixcrypto@protonmail.com>
2023-04-17 17:50:34 -0700

    initial commit

---
+++ src/generate.js
@@ -0,0 +1,58 @@
+const { spawn } = require("node:child_process");
+const fs = require("node:fs");
+
+if (!fs.existsSync("./tmp")) {
+  fs.mkdirSync("./tmp");
+}
+
+async function generateImage(prompt) {
+  let id = Math.random().toString(36).substring(7);
+
+  while (fs.existsSync(`./tmp/${id}.jpg`)) {
+    id = Math.random().toString(36).substring(7);
+  }
+
+  const outputDir = `output/${id}`;
+
+  await new Promise((resolve) => {
+    const options = ["--model", "SD-2.1", "--outdir", outputDir, prompt];
+
+    const cmd = spawn("imagine", options);
+
+    cmd.stderr.on("data", (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    cmd.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+      resolve(true);
+    });
+  });
+
+  const jpegs = fs
+    .readdirSync(`./${outputDir}/generated`)
+    .filter((file) => file.endsWith(".jpg"));
+
+  const file = `./public/${id}.jpg`;
+  fs.renameSync(`./${outputDir}/generated/${jpegs[0]}`, file);
+
+  fs.rmSync(`./${outputDir}`, { recursive: true });
+
+  // fs.open(file, undefined, (err, fd) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     fs.fsync(fd, (err) => {
+  //       if (err) {
+  //         console.log(err);
+  //       } else {
+  //         console.log("File synced");
+  //       }
+  //     });
+  //   }
+  // });
+
+  return { file };
+}
+
+module.exports = { generateImage };
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { generateImage } from "@/generate";
 
 type Data = {
-  name: string
-}
+  // prompt: string;
+  file: { file: string };
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const file = await generateImage(req.body.prompt);
+  res.status(200).json({ file });
 }
